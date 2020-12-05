46424b2ed367da8be0b4463d88f2c46b562c4473
rodolfodom <rodolfodominguez60@aragon.unam.mx>
2020-12-04 20:42:49 -0600

    I added changes to up button after revision

--- assets/scripts/modules/buttonUp.js
+++ assets/scripts/modules/buttonUp.js
@@ -1,12 +1,23 @@
-export function btnUp(btn){
-    const d = document,
-    $button = d.querySelector(btn);
-    d.addEventListener("scroll", ()=>{
-        if(scrollY > 1000){
-            $button.style.opacity = "1";
-        }
-        if(scrollY < 1000){
-            $button.style.opacity = "0";
-        }
+const d = document;
+
+export function btnUp(btn){    
+    const $button = d.querySelector(btn);
+    d.addEventListener("scroll", (e)=>{
+        let scrollTop = d.documentElement.scrollTop;
+        if(scrollTop > 1000){
+            $button.classList.remove("hidden");                                    
+        };
+        if(scrollTop < 1000){
+            $button.classList.add("hidden");
+        };
+    });
+
+    d.addEventListener("click", (e)=>{
+        if(e.target.matches(btn)){
+            scrollTo({
+                behavior:"smooth",
+                top: 0,
+            });
+        };
     });
 };
