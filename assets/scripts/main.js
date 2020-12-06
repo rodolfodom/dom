69d3dffac754f1ef535e83d73395267747edfb7f
rodolfodom <rodolfodominguez60@aragon.unam.mx>
2020-12-06 17:29:26 -0600

    I added responsive desing by JavaScript at embeded video and map

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,13 +5,16 @@ import { coundown } from "./modules/countdown.js";
 import { btnUp } from "./modules/buttonUp.js";
 import { darkMode } from "./modules/darkMode.js";
 import { ModeStorage, setValStorage } from "./modules/storage.js";
-import { videosRespLoad } from "./modules/resposive.js";
+import { embedResp } from "./modules/resposive.js";
 
 
 const d = document,
 w = window,
-fechaL = new Date(2020, 11, 5, 20, 11, 0);
-
+fechaL = new Date(2020, 11, 5, 20, 11, 0),
+desktopVideo = '<iframe width="650" height="315" src="https://www.youtube.com/embed/PdBemFos7QM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+mobileVideo = '<a href = "https://www.youtube.com/watch?v=PdBemFos7QM">Ver vídeo </a>',
+desktopMap = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30108.844035633563!2d-99.11186453935711!3d19.386226992843582!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85ce0026db097507%3A0x54061076265ee841!2sCiudad%20de%20M%C3%A9xico%2C%20CDMX!5e0!3m2!1ses!2smx!4v1607269225499!5m2!1ses!2smx" width="650" height="400" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>',
+mobileMap = '<a href = "https://www.youtube.com/watch?v=PdBemFos7QM">Ver mapa</a>';
 
 d.addEventListener("DOMContentLoaded", (e)=>{;
     setValStorage(".darkBtn");
@@ -21,13 +24,18 @@ d.addEventListener("DOMContentLoaded", (e)=>{;
     coundown(fechaL, "#countdown", "Es hoy, es hoy");
     btnUp(".upBtn");
     darkMode(".darkBtn");
-    ModeStorage(".darkBtn");  
+    ModeStorage(".darkBtn");
+    embedResp("youtube", "(min-width: 960px)", desktopVideo, mobileVideo);  
+    embedResp("gmap", "(min-width: 960px)", desktopMap, mobileMap);  
 });
 
 d.addEventListener("keydown", (e)=>{
     shortCuts(e);
     moveBall(e, "#bolita", ".tablero"); 
 });
+
+/*
+******* mo respiuesta respnsive*******
 
 w.addEventListener("load", () => {
     videosRespLoad("#embebido");
@@ -36,4 +44,4 @@ w.addEventListener("load", () => {
 w.addEventListener("resize", () => {
     videosRespLoad("#embebido");
 });
-
+*/
--- assets/scripts/modules/resposive.js
+++ assets/scripts/modules/resposive.js
@@ -1,16 +1,16 @@
 const w = window,
 d = document,
 dataInfo = [
-    {embed:'<iframe width="560" height="315" src="https://www.youtube.com/embed/PdBemFos7QM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+    {embed:'<iframe width="650" height="315" src="https://www.youtube.com/embed/PdBemFos7QM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
     url: 'https://www.youtube.com/watch?v=PdBemFos7QM',
     elemento: 'video'},
-    {embed:'<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30108.844035633563!2d-99.11186453935711!3d19.386226992843582!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85ce0026db097507%3A0x54061076265ee841!2sCiudad%20de%20M%C3%A9xico%2C%20CDMX!5e0!3m2!1ses!2smx!4v1607269225499!5m2!1ses!2smx" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>',
+    {embed:'<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30108.844035633563!2d-99.11186453935711!3d19.386226992843582!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85ce0026db097507%3A0x54061076265ee841!2sCiudad%20de%20M%C3%A9xico%2C%20CDMX!5e0!3m2!1ses!2smx!4v1607269225499!5m2!1ses!2smx" width="650" height="400" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>',
     url: 'https://goo.gl/maps/r4go2YCgjzVKBezRA',
     elemento: 'mapa'}
 ];
 
-
-export function videosRespLoad(id) {
+// ******************************** mi respuesta ********************* //
+function videosRespLoad(id) {
     const $container = d.querySelector(id);
     if(w.innerWidth > 960){
         $container.innerHTML = `${dataInfo[0].embed}\n
@@ -19,4 +19,22 @@ export function videosRespLoad(id) {
         $container.innerHTML = `<a href= "${dataInfo[0].url}"> Ver ${dataInfo[0].elemento}</a>\n
         <a href= "${dataInfo[1].url}"> Ver ${dataInfo[1].elemento}</a>`
     };
+};
+
+//***************************************** respuesta del video*************//
+
+export function embedResp(id, MQ, desktopContent, mobileContent) {
+    let breakPoint = w.matchMedia(MQ),
+    $element = d.getElementById(id);
+    
+    function responsive(e) {
+        if(e.matches){
+            $element.innerHTML = desktopContent;
+        }else{
+            $element.innerHTML = mobileContent;
+        };   
+    };
+    
+    breakPoint.addEventListener("change", responsive);
+    responsive(breakPoint);
 };
